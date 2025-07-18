881c6cb0bf3e0ad2076f66f3878d39dea868dc19
Bastian <117369801+andrxxsv122@users.noreply.github.com>
2025-07-18 11:32:21 -0400

    feat: agregar repositorio de Doctor

---
+++ Repositories/IDoctorRepository.cs
@@ -0,0 +1,14 @@
+﻿using GestionAtencionesAPI.Models;
+
+namespace GestionAtencionesAPI.Repositories
+{
+    public interface IDoctorRepository
+    {
+        public Task<IEnumerable<Doctor>> GetAllAsync();
+        public Task<Doctor?> GetByIdAsync(int id);
+        public Task<Doctor?> GetByLicenseAsync(string licenseNumber);
+        public Task<int> CreateAsync(Doctor doctor);
+        public Task<bool> UpdateAsync(Doctor doctor);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
