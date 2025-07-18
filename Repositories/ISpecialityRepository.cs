5c1f9a727fbed2b0fe41183a9d0820834ba8e723
Bastian <117369801+andrxxsv122@users.noreply.github.com>
2025-07-18 11:06:44 -0400

    "feat: agregar repositorio de Speciality"

---
+++ Repositories/ISpecialityRepository.cs
@@ -0,0 +1,13 @@
+﻿using GestionAtencionesAPI.Models;
+
+namespace GestionAtencionesAPI.Repositories
+{
+    public interface ISpecialityRepository
+    {
+        public Task<IEnumerable<Speciality>> GetAllAsync();
+        public Task<Speciality?> GetByIdAsync(int id);
+        public Task<int> CreateAsync(Speciality speciality);
+        public Task<bool> UpdateAsync(Speciality speciality);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
